7ccbf273bc104d161793e46cc9d53344a0f83c7e
Seungkyu-Han <trust1204@gmail.com>
2026-09-03 13:07:25 +0900

    feat: add custom exceptions AgentConfigException and HwpxEmptyException to HWPX agent module

--- hwpx_agent/exceptions/__init__.py
+++ hwpx_agent/exceptions/__init__.py
@@ -1,5 +1,7 @@
 from .hwpx_generate_template_exception import HwpxGenerateTemplateException
+from .agent_config_exception import AgentConfigException
 
 __all__ = [
     "HwpxGenerateTemplateException",
+    "AgentConfigException",
 ]
---
+++ hwpx_agent/exceptions/agent_config_exception.py
@@ -0,0 +1,4 @@
+class AgentConfigException(Exception):
+
+    def __init__(self, message: str):
+        super().__init__(message)
